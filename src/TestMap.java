c7332fc9e90686073b2c57274ad53107a4e7466b
DoHuyToan <dhtoan89@gmail.com>
2021-09-30 23:11:06 +0700

    ok

--- src/TestMap.java
+++ src/TestMap.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class TestMap {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestMap {
+    public static void main(String[] args) {
+        // tạo đối tượng hashMap lưu Tên SV(như là khóa key) v Tuổi theo HashMap
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Smith", 30);
+        hashMap.put("Anderson", 31);
+        hashMap.put("Lewis", 29);
+        hashMap.put("Cook", 29);
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashMap + "\n");
+
+        // tạo đối tượng treeMap lưu key theo thứ tự giảm dần đầu chữ cái aphabet
+        Map<String, Integer> treeMap = new TreeMap<>(hashMap);
+        System.out.println("Display entries in ascending order of key");
+        System.out.println(treeMap);
+
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>(16, 0.75f, true);
+        linkedHashMap.put("Smith", 30);
+        linkedHashMap.put("Anderson", 31);
+        linkedHashMap.put("Lewis", 29);
+        linkedHashMap.put("Cook", 29);
+        System.out.println("\nThe age for " + "Lewis is " + linkedHashMap.get("Lewis"));
+    }
 }
