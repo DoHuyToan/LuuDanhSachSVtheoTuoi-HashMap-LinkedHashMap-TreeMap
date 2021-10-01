91c421c35e3fecf92d0253a486dcfcf192e7955f
DoHuyToan <dhtoan89@gmail.com>
2021-10-01 16:01:29 +0700

    ok

--- src/TestMap.java
+++ src/TestMap.java
@@ -5,7 +5,7 @@ import java.util.TreeMap;
 
 public class TestMap {
     public static void main(String[] args) {
-        // tạo đối tượng hashMap lưu Tên SV(như là khóa key) v Tuổi theo HashMap
+        // tạo đối tượng hashMap lưu Tên SV(như là khóa key) v Tuổi theo HashMap, hiển thị ko theo thứ tự
         Map<String, Integer> hashMap = new HashMap<>();
         hashMap.put("Smith", 30);
         hashMap.put("Anderson", 31);
@@ -14,17 +14,20 @@ public class TestMap {
         System.out.println("Display entries in HashMap");
         System.out.println(hashMap + "\n");
 
-        // tạo đối tượng treeMap lưu key theo thứ tự giảm dần đầu chữ cái aphabet theo TreeMap
+        // tạo đối tượng treeMap lưu key theo thứ tự giảm dần đầu chữ cái aphabet theo TreeMap: ABC...XYZ, ưu tiên viết hoa
         Map<String, Integer> treeMap = new TreeMap<>(hashMap);
         System.out.println("Display entries in ascending order of key");
         System.out.println(treeMap);
 
-        // tạo đối tượng linkedHashMap để lưu danh sách tương tự HashMap và hiển thị dữ liệu
+        // tạo đối tượng linkedHashMap để lưu danh sách tương tự HashMap và hiển thị dữ liệu theo đúng thứ tự put
         Map<String, Integer> linkedHashMap = new LinkedHashMap<>(16, 0.75f, true);
         linkedHashMap.put("Smith", 30);
+        linkedHashMap.put("Cook", 29);
         linkedHashMap.put("Anderson", 31);
         linkedHashMap.put("Lewis", 29);
-        linkedHashMap.put("Cook", 29);
+
+        System.out.println("linkedHashMap");
+        System.out.println(linkedHashMap);
         System.out.println("\nThe age for " + "Lewis is " + linkedHashMap.get("Lewis"));
     }
 }
